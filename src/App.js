a4b6017681c2b3c1fdb50b7d63501424d711c745
Dongsik <ret0422@gmail.com>
2021-01-09 13:48:54 +0900

    20200109 finish #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+function Food({name, picture}){
+  return <div><h1>I Like {name}</h1>;
+  <img src={picture} />;
+  </div>
+}
+
+const foodILike = [
+  {
+    name: "kimchi",
+    img: "https://t16.pimg.jp/063/894/506/0/63894506.jpg"
+  },
+  {
+    name: "bab",
+    img: "https://en.pimg.jp/053/525/791/1/53525791.jpg"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
+      {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.img} />
+      ))}
+
     </div>
   );
 }
