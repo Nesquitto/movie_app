a18f4dd4a37f92c1c581609b352299269be011d1
Dongsik <ret0422@gmail.com>
2021-01-09 15:45:50 +0900

    one more! learning about propTypes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import Proptypes from "prop-types";
 
-
-function Food({name, picture}){
+function Food({name, picture, rating}){
   return <div><h1>I Like {name}</h1>
+  <h4>{rating}/5.0</h4>
   <img src={picture} alt={name}/>
   </div>
 }
+
+Food.propTypes = {
+ name: Proptypes.string.isRequired,
+ picture: Proptypes.string.isRequired,
+ rating: Proptypes.number
+};
 
 const foodILike = [
   {
     id: 1,
     name: "kimchi",
-    img: "https://t16.pimg.jp/063/894/506/0/63894506.jpg"
+    img: "https://t16.pimg.jp/063/894/506/0/63894506.jpg",
   },
   {
     id: 2,
     name: "bab",
-    img: "https://en.pimg.jp/053/525/791/1/53525791.jpg"
+    img: "https://en.pimg.jp/053/525/791/1/53525791.jpg",
+    rating: 5
   }
 ];
-
-function renderFood(dish){
-  console.log(dish);
-  return <Food name= {dish.name} picture= {dish.img} />
-}
 
 function App() {
   return (
@@ -30,7 +33,11 @@ function App() {
       <h1>Hello!</h1>
       {
         foodILike.map(dish => (
-          <Food key={dish.id} name={dish.name} picture = {dish.img}/>
+          <Food 
+          key={dish.id} 
+          name={dish.name} 
+          picture = {dish.img} 
+          rating = {dish.rating}/>
         ))
       }
       
