fbdc2334fa5dcdedb6f6cd46f99a8166db96153c
Dongsik <ret0422@gmail.com>
2021-01-09 17:56:02 +0900

    #4.1 Rendering the Movies
    API를 사용해서 영화 데이터를 가져오고, 영화 title을 출력하는 것까지 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -7,18 +8,28 @@ class App extends React.Component {
       movies: []
   };
   getMovies = async() => {
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
     // axios가 끝날때까지 기다렸다가 진행해라! axios는 시간이 좀 필요하기 때문이다.
+    console.log(movies);
+    this.setState({movies, isLoading: false})
   }
   componentDidMount(){
     this.getMovies();
   }
 
   render(){
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return( 
     <div>
-      {isLoading ? "Loading..." : "We are ready"}
+      {isLoading ? "Loading..." : movies.map(movie => (
+       <Movie 
+        id={movie.id} 
+        year={movie.year} 
+        title={movie.title} 
+        summary={movie.summary} 
+        poster={movie.medium_cover_image}
+        />
+      ))}
     </div>
     )
   }
