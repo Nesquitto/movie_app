734669e56cdf6d2e219f9e234b9ffe8196862ae9
Dongsik <ret0422@gmail.com>
2021-01-09 16:16:15 +0900

    #3.1 All you nedd to know about State
    Why we use setState?

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ class App extends React.Component {
   };
   add = () => {
     console.log("add");
+    this.setState(current => ({count:current.count + 1})); // current를 사용할 수 있기 때문에,
   };
   minus = () => {
     console.log("minus");
+    this.setState({count:this.state.count - 1}); // 선호하지는 않음
   };
   render(){
     return( 
