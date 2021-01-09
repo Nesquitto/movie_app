86d46f18c24f8345883df001e2e97f46f2a38847
Dongsik <ret0422@gmail.com>
2021-01-09 15:24:56 +0900

    one more push! #2.3 map Recap
    map 함수에 대해서 자세하게 배웠다.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 
 
 function Food({name, picture}){
-  return <div><h1>I Like {name}</h1>;
-  <img src={picture} />;
+  return <div><h1>I Like {name}</h1>
+  <img src={picture} alt={name}/>
   </div>
 }
 
 const foodILike = [
   {
+    id: 1,
     name: "kimchi",
     img: "https://t16.pimg.jp/063/894/506/0/63894506.jpg"
   },
   {
+    id: 2,
     name: "bab",
     img: "https://en.pimg.jp/053/525/791/1/53525791.jpg"
   }
 ];
 
+function renderFood(dish){
+  console.log(dish);
+  return <Food name= {dish.name} picture= {dish.img} />
+}
+
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      {foodILike.map(dish => (
-      <Food name={dish.name} picture={dish.img} />
-      ))}
+      {
+        foodILike.map(dish => (
+          <Food key={dish.id} name={dish.name} picture = {dish.img}/>
+        ))
+      }
+      
 
     </div>
   );
