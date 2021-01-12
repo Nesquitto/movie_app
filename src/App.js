a1d97b23016ee49a74ebce9388f1d407570360ea
Dongsik <ret0422@gmail.com>
2021-01-12 20:09:11 +0900

    2021.01.12. Finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -8,7 +9,11 @@ class App extends React.Component {
       movies: []
   };
   getMovies = async() => {
-    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
     // axios가 끝날때까지 기다렸다가 진행해라! axios는 시간이 좀 필요하기 때문이다.
     console.log(movies);
     this.setState({movies, isLoading: false})
@@ -20,18 +25,33 @@ class App extends React.Component {
   render(){
     const { isLoading, movies } = this.state;
     return( 
-    <div>
-      {isLoading ? "Loading..." : movies.map(movie => (
-       <Movie 
-        id={movie.id} 
-        year={movie.year} 
-        title={movie.title} 
-        summary={movie.summary} 
-        poster={movie.medium_cover_image}
-        />
-      ))}
-    </div>
-    )
+    <section className="container">
+      {
+        isLoading ? 
+          (
+          <div className="loader">
+            <span className="loader__text"> Loading...</span>
+          </div> 
+          ) : 
+          (
+            <div className="movies">
+              {
+                movies.map(movie => (
+                  <Movie 
+                  id={movie.id} 
+                  year={movie.year} 
+                  title={movie.title} 
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                  />
+                ))
+              }
+            </div>
+          )
+      }
+    </section>s
+    );
   }
 }
 
